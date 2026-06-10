7afc26ccc506fe89ca6c388e45a3685be20a1e7b
Mohammad Danish <12imtiyaz124@gmail.com>
2026-06-10 21:00:11 +0530

    added Maximum Subarray solution

---
+++ Problems/MaximumSubArr.java
@@ -0,0 +1,22 @@
+public class MaximumSubArr {
+
+  public int maxSubArray(int nums[]) {
+    int sum = 0;
+    int max_sum = nums[0];
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      if (sum > max_sum) {
+        max_sum = sum;
+      }
+      if (sum < 0) {
+        sum = 0;
+      }
+    }
+    return max_sum;
+  }
+
+  void main() {
+    int nums[] = { 5, 4, -1, 7, 8 };
+    System.out.println(maxSubArray(nums));
+  }
+}
