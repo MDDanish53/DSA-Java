de0b657ff7c65063e11cad25ccbdd0e4de06641d
Mohammad Danish <12imtiyaz124@gmail.com>
2026-07-25 21:05:19 +0530

    added Lowest-Common-Ancestor solution

---
+++ Problems/BinaryTree/LowestCommonAncestor.java
@@ -0,0 +1,31 @@
+// Definition for a binary tree node.
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+}
+
+public class LowestCommonAncestor {
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == null) {
+      return null;
+    }
+    if (root.equals(p) || root.equals(q)) {
+      return root;
+    }
+
+    TreeNode lans = lowestCommonAncestor(root.left, p, q);
+    TreeNode rans = lowestCommonAncestor(root.right, p, q);
+
+    if (lans == null) {
+      return rans;
+    } else if (rans == null) {
+      return lans;
+    }
+    return root;
+  }
+}
